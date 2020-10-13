a965b41dd7e7f5e30336457f920f384193a76d0e
Raul Pacheco Rodriguez <raul_vrm_2134@hotmail.com>
2020-10-13 14:06:59 -0500

    Updates

---
+++ precache-manifest.9b2314ebe97a892d63961b91f9c989a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0ee70b04f37b70a3b34e7a5d4852f2",
+    "url": "/react-code-challenge/index.html"
+  },
+  {
+    "revision": "721495f7876aae67939c",
+    "url": "/react-code-challenge/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "3b0c968110f599a74771",
+    "url": "/react-code-challenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "721495f7876aae67939c",
+    "url": "/react-code-challenge/static/js/2.a5b61284.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-code-challenge/static/js/2.a5b61284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0c968110f599a74771",
+    "url": "/react-code-challenge/static/js/main.2790a03e.chunk.js"
+  },
+  {
+    "revision": "996a1e3f144781fd3e60",
+    "url": "/react-code-challenge/static/js/runtime-main.76160a2a.js"
+  }
+]);
